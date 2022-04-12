8b0e7e4a31be5b90886566d99463471263a9941f
Joe-Watson <98327378+Joe-Watson@users.noreply.github.com>
2022-04-12 21:02:58 +0530

    project number 2

---
+++ index/index.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello bro";
+    document.getElementById("image").src = "Room.jpg";
+
+    var suman = {
+        firstName: "Joe",
+        lastName: "Watson",
+        age: 24,
+        sex: "always",
+    }
+    console.log(suman);
+}
